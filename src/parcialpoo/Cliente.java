ee6e4f86a8ba307cc2884a96739de86246acd644
Ivan Lopez <Ivan Lopez@DESKTOP-C1HSN5J>
2018-05-03 12:57:16 -0600

    agrego clase cliente

---
+++ src/parcialpoo/Cliente.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+/**
+ *
+ * @author Ivan Lopez
+ */
+public class Cliente {
+    private Nombre nombre;
+    private String DUI;
+    private String telefono;
+    private String tarjetacredito;
+    
+    public Cliente(){
+        
+    }
+    
+    public void SetNombre(Nombre nombre){
+      this.nombre= nombre;  
+    }
+    
+    public void SetDUI(String DUI){
+        this.DUI= DUI;
+    }
+    
+    public void SetTelefono(String telefono){
+        this.telefono= telefono;
+    }
+    
+    public void SetTarjetacredito(String tarjetacredito){
+        this.tarjetacredito= tarjetacredito;
+    }
+    
+    public Nombre GetNombre(){
+        return nombre;
+    }
+    
+    public String GetDUI(){
+        return DUI;
+    }
+    
+    public String GetTelefono(){
+        return telefono;
+    }
+    
+    public String GetTarjetacredito(){
+        return tarjetacredito;
+    }
+}
