f069aa0711567bca13e93398574d032fa6e33795
Ivan Lopez <Ivan Lopez@DESKTOP-C1HSN5J>
2018-05-03 17:49:53 -0600

    agrego clase administrador

---
+++ src/parcialpoo/Administrador.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+/**
+ *
+ * @author Ivan Lopez
+ */
+public class Administrador {
+    public void modificarpreciohp(int precio){
+        habitacion.modificarpreciohp;    
+    }
+    
+    public void modificarpreciohi(int precio){
+        habitacion.modificarpreciohi;    
+    }
+    
+    public void modificarpreciop (int precio){
+        paquete.modificarpreciop
+    }
+    
+    public void modificarestadoh(){
+        
+    }
+    
+    public void agregarpiso(){
+        
+    }
+    
+    public void agregarreserva(){
+        
+    }
+}
