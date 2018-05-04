43d7fa80966e543043d8e42814e4bc2f506b89b5
Ivan Lopez <Ivan Lopez@DESKTOP-C1HSN5J>
2018-05-03 19:26:21 -0600

    cambio atributos a static

---
+++ src/parcialpoo/Habitacionp.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+/**
+ *
+ * @author Jorge Orellana <00103717@uca.edu.sv>
+ */
+public class Habitacionp {
+    private int numero;
+    private static int precio;
+    private static String tipo="simple";
+
+    public Habitacionp() {
+        
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public static void setPrecio(int precio) {
+        Habitacionp.precio = precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public static void setTipo(String tipo) {
+        Habitacionp.tipo = tipo;
+    }
+    
+    
+    
+}
