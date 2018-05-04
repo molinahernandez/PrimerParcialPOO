4489be516d02528149179b0f40748bc3a2ac7c3c
Ivan Lopez <Ivan Lopez@DESKTOP-C1HSN5J>
2018-05-03 19:26:45 -0600

    agrego clase habitacioni

---
+++ src/parcialpoo/Habitacioni.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+/**
+ *
+ * @author Jorge Orellana <00103717@uca.edu.sv>
+ */
+public class Habitacioni {
+    private int numero;
+    private static int precio;
+    private static String tipo="doble";
+
+    public Habitacioni() {
+        
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public static void setPrecio(int precio) {
+        Habitacioni.precio = precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public static void setTipo(String tipo) {
+        Habitacioni.tipo = tipo;
+    }
+    
+    
+    
+}
