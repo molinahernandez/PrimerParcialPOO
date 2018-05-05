0635781fe9189d5163cb0a5951d74d5fa1929853
Ivan Lopez <Ivan Lopez@DESKTOP-C1HSN5J>
2018-05-05 09:59:36 -0600

    crear clase hotel

---
+++ src/parcialpoo/Hotel.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ivan Lopez
+ */
+public class Hotel {
+    
+    
+    public static void Crearhotel(){
+    String[]Abecedario={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    HashMap<String,Pisop>hotel= new HashMap<>();
+    
+
+    for(int i=0;i<6;i++){
+        Pisop piso= new Pisop(Abecedario[i]);
+        piso.crearpiso();
+        hotel.put(Abecedario[i], piso);    
+    }
+    }
+    
+    
+}
