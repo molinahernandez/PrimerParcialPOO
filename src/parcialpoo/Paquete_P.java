93436079a61d566c39534e4e3331dba2a4ebbf93
Ivan Lopez <Ivan Lopez@DESKTOP-C1HSN5J>
2018-05-06 17:32:35 -0600

    creo subclase paquete_p

---
+++ src/parcialpoo/Paquete_P.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+/**
+ *
+ * @author Ivan Lopez
+ */
+public class Paquete_P extends Paquete{
+    final String tipo="premium";
+    
+}
