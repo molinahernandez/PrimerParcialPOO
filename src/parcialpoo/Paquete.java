a3d86810650b9c3eb2072f693b8467f66dc7e002
jorge <jorge@LAPTOP-GTMUDF2B>
2018-05-08 11:43:56 -0600

    tipo1  y tipo2

--- src/parcialpoo/Paquete.java
+++ src/parcialpoo/Paquete.java
@@ -10,35 +10,45 @@ package parcialpoo;
  * @author Cristian Hernandez
  */
 public class Paquete {
-    //public static String tipo;
-    protected static String Servicios;
-    protected static int precios;
+    private static String tipo1 = "Premium";
+    private static String tipo2 = "Basico";
+    private static String Servicios;
+    private static int precios;
 
-    public Paquete() {
+    public static String getTipo1() {
+        return tipo1;
     }
 
-    /*public static String getTipo() {
-        return tipo;
-    }*/
+    public static void setTipo1(String aTipo1) {
+        tipo1 = aTipo1;
+    }
+
+    public static String getTipo2() {
+        return tipo2;
+    }
+
+    public static void setTipo2(String aTipo2) {
+        tipo2 = aTipo2;
+    }
 
     public static String getServicios() {
         return Servicios;
+    }
+
+    public static void setServicios(String aServicios) {
+        Servicios = aServicios;
     }
 
     public static int getPrecios() {
         return precios;
     }
 
-    /*public static void setTipo(String tipo) {
-        Paquete.tipo = tipo;
-    }*/
-
-    public static void setServicios(String Servicios) {
-        Paquete.Servicios = Servicios;
+    public static void setPrecios(int aPrecios) {
+        precios = aPrecios;
     }
 
-    public static void setPrecios(int precios) {
-        Paquete.precios = precios;
+    public Paquete() {
     }
+
     
 }
