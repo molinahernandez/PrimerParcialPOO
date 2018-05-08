f3c762ca9fde2490af22520855d2912e55c5de91
chihe <chihe@192.168.2.7>
2018-05-08 09:34:46 -0600

    Agrear clase ReistroR y Hashmap

---
+++ src/parcialpoo/RegistroR.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Cristian Hernandez
+ */
+public class RegistroR {
+    public static HashMap<Integer,Reservacion>registro= new HashMap<>();   
+}
