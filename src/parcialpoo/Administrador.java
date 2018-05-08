0ec36fe706cb84b894961362e3ab5825ba6b04e2
jorge <jorge@LAPTOP-GTMUDF2B>
2018-05-08 11:43:21 -0600

    Mostrar y crear reservacion

--- src/parcialpoo/Administrador.java
+++ src/parcialpoo/Administrador.java
@@ -7,6 +7,7 @@ package parcialpoo;
 
 /*import java.util.HashMap;*/
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -56,17 +57,66 @@ public class Administrador {
         Hotel.añadirpiso();
     }
     
-    public void crearreservacion(String nombre, String apellido, String DUI, String telefono, String tarjetacredito, String tipopaquete){
-        nombre1.SetNombre(nombre);
+    public static void crearreservacion(int KeyR){
+        Reservacion reservacion1 = new Reservacion(); 
+        String nombre, apellido, DUI, telefono, tarjetacredito, tipopaquete;
+        
+        Scanner n=new Scanner(System.in);
+        Nombre N1 = new Nombre();
+        Cliente C1 = new Cliente();
+        
+        System.out.println("Escriba su nombre: ");
+        nombre=n.nextLine();
+        
+        N1.SetNombre(nombre);
+        
+        Scanner ap=new Scanner(System.in);
+        
+        System.out.println("Escriba su apellido: ");
+        apellido=ap.nextLine();
+        
+        N1.SetApellido(apellido);
+        C1.SetNombre(N1);
+        
+        Scanner d=new Scanner(System.in);
+        
+        System.out.println("Escriba su DUI: ");
+        DUI=d.nextLine();
+        
+        C1.SetDUI(DUI);
+        
+        Scanner t=new Scanner(System.in);
+        
+        System.out.println("Escriba su telefono: ");
+        telefono=t.nextLine();
+        
+        C1.SetTelefono(telefono);
+        
+        Scanner ta=new Scanner(System.in);
+        
+        System.out.println("Escriba su tarjeta de credito: ");
+        tarjetacredito=ta.nextLine();
+        
+        C1.SetTarjetacredito(tarjetacredito);
+        
+        /*Scanner ti=new Scanner(System.in);
+        Paquete P1 = new Paquete();
+        
+        System.out.println("Escriba el tipo de paquete: ");
+        tipopaquete=ti.nextLine();
+        P1.*/
+        
+        
+        /*nombre1.SetNombre(nombre);
         nombre1.SetApellido(apellido);
         cliente.SetNombre(nombre1);
         cliente.SetDUI(DUI);
         cliente.SetTarjetacredito(tarjetacredito);
-        cliente.SetTelefono(telefono);
+        cliente.SetTelefono(telefono);*/
         
         int a,b;
         Scanner entrada= new Scanner(System.in);
-        System.out.println("1=paquete premium, 2=paquete basico, 3=no tiene paquete");
+        System.out.println("1=paquete premium, 2=paquete basico, 3=no quiero paquete");
         a=entrada.nextInt();
         switch(a){
             case 1:
@@ -74,16 +124,16 @@ public class Administrador {
                 b=entrada.nextInt();
                 switch(b){
                     case 1:
-                        reservacion.setCliente(cliente);
-                        reservacion.setPaquete(tipopaquete);
-                        reservacion.setPrecio(Habitacionp.getPrecio() + Paquete_P.getPrecios());
-                        reservacion.setServiciospaquete(Paquete_P.getServicios());
+                        reservacion1.setCliente(C1);
+                        reservacion1.setPaquete(Paquete.getTipo1());
+                        reservacion1.setPrecio(Habitacionp.getPrecio() + Paquete_P.getPrecios());
+                        reservacion1.setServiciospaquete(Paquete_P.getServicios());
                         break;
                     case 2:
-                        reservacion.setCliente(cliente);
-                        reservacion.setPaquete(tipopaquete);
-                        reservacion.setPrecio(Habitacioni.getPrecio() + Paquete_P.getPrecios());
-                        reservacion.setServiciospaquete(Paquete_P.getServicios());
+                        reservacion1.setCliente(C1);
+                        reservacion1.setPaquete(Paquete.getTipo1());
+                        reservacion1.setPrecio(Habitacioni.getPrecio() + Paquete_P.getPrecios());
+                        reservacion1.setServiciospaquete(Paquete_P.getServicios());
                         break;
                 }
                 break;
@@ -92,16 +142,16 @@ public class Administrador {
                 b=entrada.nextInt();
                 switch(b){
                     case 1:
-                        reservacion.setCliente(cliente);
-                        reservacion.setPaquete(tipopaquete);
-                        reservacion.setPrecio(Habitacionp.getPrecio() + Paquete_S.getPrecios());
-                        reservacion.setServiciospaquete(Paquete_S.getServicios());
+                        reservacion1.setCliente(C1);
+                        reservacion1.setPaquete(Paquete.getTipo2());
+                        reservacion1.setPrecio(Habitacionp.getPrecio() + Paquete_S.getPrecios());
+                        reservacion1.setServiciospaquete(Paquete_S.getServicios());
                         break;
                     case 2:
-                        reservacion.setCliente(cliente);
-                        reservacion.setPaquete(tipopaquete);
-                        reservacion.setPrecio(Habitacioni.getPrecio() + Paquete_S.getPrecios());
-                        reservacion.setServiciospaquete(Paquete_S.getServicios());
+                        reservacion1.setCliente(C1);
+                        reservacion1.setPaquete(Paquete.getTipo2());
+                        reservacion1.setPrecio(Habitacioni.getPrecio() + Paquete_S.getPrecios());
+                        reservacion1.setServiciospaquete(Paquete_S.getServicios());
                         break;
                 }
                 break; 
@@ -110,18 +160,29 @@ public class Administrador {
                 b=entrada.nextInt();
                 switch(b){
                     case 1:
-                        reservacion.setCliente(cliente);
-                        reservacion.setPrecio(Habitacionp.getPrecio());
+                        reservacion1.setCliente(C1);
+                        reservacion1.setPrecio(Habitacionp.getPrecio());
                         break;
                     case 2:
-                        reservacion.setCliente(cliente);
-                        reservacion.setPrecio(Habitacioni.getPrecio());
+                        reservacion1.setCliente(C1);
+                        reservacion1.setPrecio(Habitacioni.getPrecio());
                         break;
                 }
                 break;
         }
-        
+        RegistroR.registro.put(KeyR + 1, reservacion1);
     }
     
+    
+    public static void MostrarReservacion(){
+                //Se crea una variable Keys que guarde todas las key de hotel
+        Set<Integer> Keys = RegistroR.registro.keySet();
+        
+        for(Integer Key:Keys){
+            Reservacion valor = RegistroR.registro.get(Key);
+            System.out.println(valor.cliente.GetNombre().GetNombre() + valor.cliente.GetNombre().GetApellido()+
+            valor.getTipopaquete() + valor.habitacionp + valor.habitacioni + valor.precio);
+        }
+    }
     
 }
