7571b534ab1fd68624896b7cd74f5e2155c63a09
jorge <jorge@LAPTOP-GTMUDF2B>
2018-05-08 11:44:30 -0600

    ejecutando

--- src/parcialpoo/ParcialPOO.java
+++ src/parcialpoo/ParcialPOO.java
@@ -15,10 +15,15 @@ public class ParcialPOO {
      * @param args the command line arguments
      */
     public static void main(String[] args) { 
+        int KeyR = 0;
         CrearHotel hotel = new CrearHotel();
         
         Hotel.Crearhotel();
         Hotel.MostrarHotel();
+        
+        Administrador.crearreservacion(KeyR);
+        Administrador.MostrarReservacion();
+        
         Hotel.añadirpiso();
         Hotel.MostrarHotel();
     }
